e8e8e2afa59c15c6047d95eab4931527514f9e95
zrozez <r.sakhizova@gmail.com>
2021-09-18 20:28:07 +0600

    Fix mistakes

--- tester/urls.py
+++ tester/urls.py
@@ -19,13 +19,6 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),
-    path('rest-auth/', include('rest_auth.urls')),
-    path('api/v1/swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),    
-]
-
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -39,3 +32,10 @@ schema_view = get_schema_view(
    public=True,
    permission_classes=(permissions.AllowAny,),
 )
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+    path('rest-auth/', include('rest_auth.urls')),
+    path('api/v1/swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),    
+]
